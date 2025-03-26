7ceccf74ba72a84193924e3b3606a80e93e85b8f
its.john <giovannialesi87@icloud.com>
2025-03-26 18:53:45 +0100

    query group by

--- quesries.sql
+++ quesries.sql
@@ -9,7 +9,10 @@ FROM `courses`
 WHERE `cfu` > 10;
 
 --Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT `id`, `name`, `surname`, `date_of_birth`, TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE())
+FROM `students`
+WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30
+ORDER BY `age`
 
 -- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 -- laurea (286)
@@ -17,6 +20,10 @@ SELECT *
 FROM `courses`
 WHERE `period` AND`year`= 1;
 --(non è giusto)
+SELECT *
+FROM `courses`
+WHERE `period` 
+AND`year`LIKE "1%";
 
 -- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 -- 20/06/2020 (21)
@@ -33,3 +40,32 @@ ORDER BY `hour` DESC;
 SELECT *
  FROM `teachers`
  WHERE `phone` IS NULL;
+
+
+
+
+-------------------Query con GROUP BY
+
+-- Contare quanti iscritti ci sono stati ogni anno
+ SELECT YEAR(`enrolment_data`), COUNT(*)
+FROM `students`
+GROUP BY YEAR(`enrolment_data`);
+
+--Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT `office_address`, COUNT(*)
+FROM `teachers`
+GROUP BY `office_address`;
+
+-- Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id`, AVG(`vote`)
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+-- Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(*)
+FROM `degrees`
+GROUP BY `department_id`;
+
+--------Query con JOIN
+--Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
